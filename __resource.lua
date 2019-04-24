215590c26c361f48c77195ec704d5bc1a33d5e95
SFL-Master <48449300+SFL-Master@users.noreply.github.com>
2019-04-24 13:05:58 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,9 +2,10 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Peds Script by [SF.L] Master'
 
-version '1.0.0'
+version '1.5.1'
 
 client_scripts {
-	'config.lua',
-	'client/main.lua'
+
+	'client/peds.lua',
+	'config.lua'
 }
